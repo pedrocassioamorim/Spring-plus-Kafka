b1d6397a75a686a1bbad4edc40208cc39547cb26
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-07-01 22:10:50 -0300

    Creating module OrderProducer for Kafka Streaming

---
+++ OrderProducer/src/test/java/net/producer/order/OrderProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.producer.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
