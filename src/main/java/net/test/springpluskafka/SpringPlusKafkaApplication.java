9d480b23f35d75edc248ab0dab5843b66daa680e
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-07-01 21:41:45 -0300

    Initiating project expand Kafka concepts

---
+++ src/main/java/net/test/springpluskafka/SpringPlusKafkaApplication.java
@@ -0,0 +1,13 @@
+package net.test.springpluskafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPlusKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPlusKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/test/springpluskafka/SpringPlusKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.test.springpluskafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPlusKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
